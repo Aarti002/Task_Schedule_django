58827dfbfcb7dfd58923f97ce5f31c0c279a7036
Aarti002 <aartikumarisingh120@gmail.com>
2023-11-30 17:49:29 +0530

    Email settings added in settings.py file

--- task_schedule/settings.py
+++ task_schedule/settings.py
@@ -126,6 +126,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# EMAIL SETTINGS
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIl_PORT = 587
+EMAIL_HOST_USER = "aartikumarisingh3002@gmail.com"
+EMAIL_HOST_PASSWORD = "qnijirhtmjbgeass"
+EMAIL_USE_TLS = True
+
+
 # CELERY SETTINGS
 
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
